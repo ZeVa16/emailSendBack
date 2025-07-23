1d2f483ffee5a81a791acadca6ad6d693169ecfe
ZeVa16 <arturo1414zv@hotmail.com>
2025-07-22 23:25:19 -0600

    FIX env variables

--- src/main/java/com/example/sendEmail/config/AplicationConfig.java
+++ src/main/java/com/example/sendEmail/config/AplicationConfig.java
@@ -1,5 +1,22 @@
 package com.example.sendEmail.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class AplicationConfig {
 
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll(); // permite todas las rutas
+        return http.build();
+    }
 }
--- src/main/java/com/example/sendEmail/controllers/Impl/UserControllerImpl.java
+++ src/main/java/com/example/sendEmail/controllers/Impl/UserControllerImpl.java
@@ -1,15 +1,34 @@
 package com.example.sendEmail.controllers.Impl;
 
+import com.example.sendEmail.controllers.UserController;
+import com.example.sendEmail.dtos.ApiResponse;
+import com.example.sendEmail.dtos.userDtos.UserResponse;
 import com.example.sendEmail.services.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class UserControllerImpl {
+public class UserControllerImpl implements UserController {
 
     private final UserService userService;
     public UserControllerImpl(UserService userService) {
         this.userService = userService;
+    }
+
+    @Override
+    public ResponseEntity<ApiResponse<List<UserResponse>>> getAllUsers(){
+        List<UserResponse> users = userService.getAllUsers();
+        ApiResponse<List<UserResponse>> apiResponse = ApiResponse.<List<UserResponse>>builder()
+                .message("Users obtainerd succesfully")
+                .data(users)
+                .status(HttpStatus.OK)
+                .build();
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+
+
     }
 
 
