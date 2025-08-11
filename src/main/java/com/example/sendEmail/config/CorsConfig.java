a5c481cb0280f0f79fb406bda174f4fb78e7b78e
ZeVa16 <arturo1414zv@hotmail.com>
2025-08-11 01:15:28 -0600

    ADD cors config for front

--- src/main/java/com/example/sendEmail/config/CorsConfig.java
+++ src/main/java/com/example/sendEmail/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5174")); // tu frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:5174","https://crispykitchen-one.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
